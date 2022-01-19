8c2d5ec648be083cbcde9ad8ddb5e83f14d09287
Henson Husada <hensonhusada@gmail.com>
2022-01-19 16:04:15 +0700

    Commit 2 Henson

--- apps/schemas/SchemaCIF.py
+++ apps/schemas/SchemaCIF.py
@@ -1,18 +1,53 @@
+import datetime
 from pydantic import BaseModel
 from typing import Optional, List
 
 
 class RequestCIF(BaseModel):
     cif: str = None
+    idno: str = None
 
 
 class CIF(BaseModel):
     loanid: str = None
+    loan_type : str = None
+    loan_status : str = None
     cif: str  = None
-    tenor: int  = None
-    amount: int  = None
-    limit: int  = None
+    loan_tenure : int = None
+    loan_amount: int  = None
+    interest : int = None
+    idno : str = None
+    fname : str = None
+    lname : str = None
+    dob : datetime.date = None
+    gender : str = None
+    marital_status : str = None
+    income : int = None
+    phone : str = None
+    email : str = None
+    isphoneverified : str = None
+    isemailverified : str = None
+    createdate : datetime.date = None
+    updatedate : datetime.date = None
+    source : str = None
+    # limit: int  = None
 
 
 class ResponseCIF(BaseModel):
     cif_list: List[CIF]
+
+class Customer(BaseModel):
+    idno : str = None
+    fname : str = None
+    lname : str = None
+    dob : datetime.date = None
+    age : int = None
+    gender : str = None
+    marital_status : str = None
+    income : int = None
+    phone : str = None
+    email : str = None
+
+
+class ResponseCustomer(BaseModel):
+    customer : List[Customer]
