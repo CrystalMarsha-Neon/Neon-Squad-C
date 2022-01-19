525051dd069d1a29b6498c1e7dd2907e27e8ab99
RifkiFzNEON <leminerale6666@gmail.com>
2022-01-19 15:37:02 +0700

    Fix bug on LoanController

--- apps/schemas/SchemaCIF.py
+++ apps/schemas/SchemaCIF.py
@@ -28,14 +28,29 @@ class CIF(BaseModel):
     source: str = None,
 
 class Loan_by_Cif(BaseModel):
-    cif: str  = None
-    loanid: str = None
-    tenor: int  = None
-    amount: int  = None
-    loan_type : str = None
-    loan_status : str = None
-
+    loan_status: int = None,
+    loan_amount: int = None,
+    # loan_tenure: int = None,
+    # interest: int = None,
+    cif: int = None,
+    idno: str = None,
+    fname: str = None,
+    lname: str = None,
+    # dob: str = None,
+    gender: str = None,
+    # marital: str = None,
+    income: int = None,
+    phone: str = None,
+    email: str = None,
+    # isphoneverified: int = None,
+    # isemailverified: int = None,
+    # createdate: str = None,
+    # updatedate: str = None,
+    source: str = None,
 
 
 class ResponseCIF(BaseModel):
     cif_list: List[CIF]
+
+class ResponseCIF1(BaseModel):
+    cif_list: List[Loan_by_Cif]
