71b53c6640c6e80507bcd06e5ff89114a42e0b0f
RifkiFzNEON <leminerale6666@gmail.com>
2022-01-19 16:03:58 +0700

    Update IFRoute, masi coba coba

--- apps/routers/InformationRouter.py
+++ apps/routers/InformationRouter.py
@@ -14,3 +14,9 @@ async def home():
 @router.get("/ping")
 async def ping():
     return APPS_INFORMATION["version"]
+
+@router.get("/rifki1")
+async def rifki1():
+    result = loan.rifki(input_data=input_data)
+    response.status_code = result.status
+    return result
