05db5b8c160466d2f8a1c3f57861c5374097f15a
RifkiFzNEON <leminerale6666@gmail.com>
2022-01-19 16:07:31 +0700

    gajadi update, balik lagi karena masi error

--- apps/routers/InformationRouter.py
+++ apps/routers/InformationRouter.py
@@ -14,9 +14,3 @@ async def home():
 @router.get("/ping")
 async def ping():
     return APPS_INFORMATION["version"]
-
-@router.get("/rifki1")
-async def rifki1():
-    result = loan.rifki(input_data=input_data)
-    response.status_code = result.status
-    return result
