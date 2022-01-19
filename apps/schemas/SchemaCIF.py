ac9078bd0ab7f952805a25a705876855245f5628
RifkiFzNEON <leminerale6666@gmail.com>
2022-01-19 14:59:21 +0700

    Update SchemaCIF

--- apps/schemas/SchemaCIF.py
+++ apps/schemas/SchemaCIF.py
@@ -11,8 +11,27 @@ class CIF(BaseModel):
     cif: str  = None
     tenor: int  = None
     amount: int  = None
-    limit: int  = None
-
+    # limit: int  = None
+    loan_type : str = None
+    loan_status : str = None
+    cif: str  = None
+    loan_tenure : int = None
+    loan_amount: int  = None
+    interest : int = None
+    idno : str = None
+    fname : str = None
+    lname : str = None
+    dob : str = None
+    gender : str = None
+    marital_status : str = None
+    income : int = None
+    phone : str = None
+    email : str = None
+    isphoneverified : str = None
+    isemailverified : str = None
+    createdate : str = None
+    updatedate : str = None
+    source : str = None
 
 class ResponseCIF(BaseModel):
     cif_list: List[CIF]
